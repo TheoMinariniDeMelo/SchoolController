c5c7136a1eee1c621aff9e1335b6983ff58f5ac8
TheoMinarini <=>
2023-09-11 20:04:06 -0300

    Cherry-pick com successo

--- domains/src/main/java/school/io/peoples/People.java
+++ domains/src/main/java/school/io/peoples/People.java
@@ -1,6 +1,9 @@
 package school.io.peoples;
 
+import school.io.address.Address;
 import school.io.address.AddressID;
+import school.io.role.Role;
+import school.io.role.RoleID;
 import school.io.transport.Transport;
 
 import java.util.UUID;
@@ -10,6 +13,61 @@ public class People {
     protected String name;
     protected Transport transport;
     protected String telephone;
-    protected AddressID address;
-    protected RoleID role;
+    protected Address address;
+    protected Role role ;
+
+    public People() {
+    }
+
+    public People(String name, Transport transport, String telephone, Address address, Role role) {
+        this.name = name;
+        this.transport = transport;
+        this.telephone = telephone;
+        this.address = address;
+        this.role = role;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Transport getTransport() {
+        return transport;
+    }
+
+    public void setTransport(Transport transport) {
+        this.transport = transport;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
--- infrastructure/src/main/java/school/io/models/Address.java
+++ infrastructure/src/main/java/school/io/models/Address.java
@@ -1,9 +1,7 @@
 package school.io.models;
 
-import java.util.UUID;
 
 public class Address {
-    protected UUID id;
     protected String road;
     protected String neighborhood;
     protected Integer number;
